a3406bedb3beeaebe1748a7fac8ce0d47e48ee8c
mrsamia <15203060@iubat.edu>
2023-04-10 12:21:45 +0600

    SRM | ShopNowBtn

--- src/Components/HeroSlider.js
+++ src/Components/HeroSlider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from "react-slick";
+import ShopNowBtn from './ShopNowBtn';
 function HeroSlider(props) {
     const settings = {
         dots: true,
@@ -12,10 +13,24 @@ function HeroSlider(props) {
         <div class="container mx-auto">
 
             <Slider {...settings}>
-                <div>
+                <div class="relative">
+
                     <img src={require('../Images/slider-01.webp')} />
+                    <div class="absolute inset-y-1/3 ">
+                        <div>
+                            <p class="text-lg text-[#f0a843] text-left font-medium pl-16">First 10 sales 10% offer</p>
+                        </div>
+                        <div className='pt-5'>
+                            <h2 class="text-[50px] text-[#232323] font-semibold text-left  pl-16 leading-none">Enjoying<br></br>grocery</h2>
+                        </div>
+                        <div className='pt-8'>
+                            <ShopNowBtn/>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div>
+                <div class="">
                     <img src={require('../Images/slider-02.webp')} />
                 </div>
                 <div>
---
+++ src/Components/ShopNowBtn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ShopNowBtn(props) {
+    return (
+       
+            <button class='px-7 bg-[#f0a843] text-white py-2.5 rounded text-md font-semibold '>shop now</button>
+      
+    );
+}
+
+export default ShopNowBtn;
